f5773e587e694bb23411c6414a56f4ff804886ff
SharaVigneswaran <154065634+SharaVigneswaran@users.noreply.github.com>
2024-05-14 14:34:56 +0200

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -71,4 +71,5 @@ if show_history and st.session_state.history:
 ############ ADDITIONAL VISUAL ELEMENTS ############
 
 # Adding a footer image or branding
-st.image("images/Logo.jpeg", caption="LogoRank", use_column_width=True)
+with c2: 
+    st.image("images/Logo.jpeg", caption="LogoRank", use_column_width=True)
