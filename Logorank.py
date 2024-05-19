9ffb901761b081b31a8d21f35acd6696854caf80
SharaVigneswaran <154065634+SharaVigneswaran@users.noreply.github.com>
2024-05-19 18:24:10 +0200

    Update Logorank.py

--- Logorank.py
+++ Logorank.py
@@ -35,13 +35,12 @@ with c2:
     """)
 
 ############ 4. APP FUNCTIONALITY ############
-
 # Function to load the model and tokenizer from GitHub
-@st.cache(allow_output_mutation=True)
+@st.cache_resource(allow_output_mutation=True)
 def load_model_and_tokenizer():
     model_url = "https://github.com/SharaVigneswaran/Data-Science-Machine-Learning-Project/raw/main/saved_model/model.safetensors"
     config_url = "https://github.com/SharaVigneswaran/Data-Science-Machine-Learning-Project/raw/main/saved_model/config.json"
-    tokenizer_dir_url = "https://github.com/SharaVigneswaran/Data-Science-Machine-Learning-Project/raw/main/saved_model/tokenizer/"
+    tokenizer_url = "https://github.com/SharaVigneswaran/Data-Science-Machine-Learning-Project/raw/main/saved_model/tokenizer/"
 
     model = AutoModelForSequenceClassification.from_pretrained(config_url, state_dict=torch.hub.load_state_dict_from_url(model_url))
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_url)
