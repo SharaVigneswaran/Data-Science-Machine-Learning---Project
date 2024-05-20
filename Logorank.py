ac6a9df51187b06b8ced14d1369863c47f802b6d
SharaVigneswaran <154065634+SharaVigneswaran@users.noreply.github.com>
2024-05-20 18:39:09 +0200

    Update Logorank.py

--- Logorank.py
+++ Logorank.py
@@ -4,12 +4,17 @@ from transformers import CamembertTokenizer, CamembertForSequenceClassification
 import torch
 import time
 
-# Load your custom tokenizer and model
-tokenizer = CamembertTokenizer.from_pretrained('saved_model')  # Provide directory path
-model = CamembertForSequenceClassification.from_pretrained('saved_model', num_labels=6)  # Adjust num_labels
+# Load the tokenizer
+tokenizer = CamembertTokenizer.from_pretrained('saved_model')
+
+# Load the model configuration
+config = AutoConfig.from_pretrained('saved_model/config.json')
+
+# Initialize the model with the configuration
+model = CamembertForSequenceClassification.from_config(config)
 
 # Load the model weights
-state_dict = torch.load('saved_model/model.safetensors')
+state_dict = torch.load('saved_model/model.safetensors', map_location=torch.device('cpu'))
 model.load_state_dict(state_dict, strict=False)
 model.eval()
 
